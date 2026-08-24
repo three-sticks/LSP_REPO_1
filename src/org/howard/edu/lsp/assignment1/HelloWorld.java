c92faf161d241dcd49e99a0406c4c0d3271bf860
three-sticks <rhardy2020@gmail.com>
2026-08-24 00:57:30 -0400

    Hello World plus ReadME created

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Robert Hardy III
+ */
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World!!");
+    }
+}
